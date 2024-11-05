9ae5e040643f74f1ad353909376c064a9f58d563
41088938 <230292130@stu.vtc.edu.hk>
2024-11-05 16:32:21 +0800

    changeScene
    簡陋地切換至開車Scene

---
+++ testMovement/Assets/Control.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("GameScene_tester");
+    }
+}
+
+
